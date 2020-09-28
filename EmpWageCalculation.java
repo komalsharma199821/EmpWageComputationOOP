d9d63f9747c5e6cc917735e97607de2dc08797aa
Komal Sharma <komal.sharma@catseyesystems.com>
2020-09-28 22:51:23 +0530

    Employee Wage Calculation UC 1

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -1,10 +1,33 @@
+import java.util.*;
 public class EmpWageCalculation {
+     //use case 1
+    public static int generateRandomNumber(){
+         double x=Math.random();
+        int y = (int) Math.round(x);
+        return y;
+    }
+    
+    public static String presenteeCheckForEmployee(int value){
+        if(value==1){
+            return "Employee is Present" ;
+        }else{
+            return "Employee is Absent" ;
+        }
+    }
+    
     public static void main(String args[]) {
-      
+       
       System.out.println("Welcome To Employee Wage Calculation Problem");
+      // use case 1
+      int randomValue = generateRandomNumber();
+      
+      String presenteeCheck = presenteeCheckForEmployee(randomValue);
+      System.out.println("Is Employee Present ? "+presenteeCheck);
+      
+     
+      
     }
 }
-
 
 
 
